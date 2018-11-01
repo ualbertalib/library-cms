66fa50f4ebdd6c39acc05067f05c22e1277976f2
nnunn <natasha@nnunn.ca>
2018-11-01 10:58:25 -0600

    changing database config file to specify mysql

--- db/schema.rb
+++ db/schema.rb
@@ -12,33 +12,33 @@
 
 ActiveRecord::Schema.define(version: 2018_11_01_154832) do
 
-  create_table "comfy_cms_categories", force: :cascade do |t|
+  create_table "comfy_cms_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "site_id", null: false
     t.string "label", null: false
     t.string "categorized_type", null: false
     t.index ["site_id", "categorized_type", "label"], name: "index_cms_categories_on_site_id_and_cat_type_and_label", unique: true
   end
 
-  create_table "comfy_cms_categorizations", force: :cascade do |t|
+  create_table "comfy_cms_categorizations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "category_id", null: false
     t.string "categorized_type", null: false
     t.integer "categorized_id", null: false
     t.index ["category_id", "categorized_type", "categorized_id"], name: "index_cms_categorizations_on_cat_id_and_catd_type_and_catd_id", unique: true
   end
 
-  create_table "comfy_cms_files", force: :cascade do |t|
+  create_table "comfy_cms_files", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "site_id", null: false
     t.string "label", default: "", null: false
-    t.text "description", limit: 2048
+    t.text "description"
     t.integer "position", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["site_id", "position"], name: "index_comfy_cms_files_on_site_id_and_position"
   end
 
-  create_table "comfy_cms_fragments", force: :cascade do |t|
+  create_table "comfy_cms_fragments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "record_type"
-    t.integer "record_id"
+    t.bigint "record_id"
     t.string "identifier", null: false
     t.string "tag", default: "text", null: false
     t.text "content", limit: 16777215
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2018_11_01_154832) do
     t.index ["record_type", "record_id"], name: "index_comfy_cms_fragments_on_record_type_and_record_id"
   end
 
-  create_table "comfy_cms_layouts", force: :cascade do |t|
+  create_table "comfy_cms_layouts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "site_id", null: false
     t.integer "parent_id"
     t.string "app_layout"
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 2018_11_01_154832) do
     t.index ["site_id", "identifier"], name: "index_comfy_cms_layouts_on_site_id_and_identifier", unique: true
   end
 
-  create_table "comfy_cms_pages", force: :cascade do |t|
+  create_table "comfy_cms_pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "site_id", null: false
     t.integer "layout_id"
     t.integer "parent_id"
@@ -87,7 +87,7 @@ ActiveRecord::Schema.define(version: 2018_11_01_154832) do
     t.index ["site_id", "full_path"], name: "index_comfy_cms_pages_on_site_id_and_full_path"
   end
 
-  create_table "comfy_cms_revisions", force: :cascade do |t|
+  create_table "comfy_cms_revisions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "record_type", null: false
     t.integer "record_id", null: false
     t.text "data", limit: 16777215
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 2018_11_01_154832) do
     t.index ["record_type", "record_id", "created_at"], name: "index_cms_revisions_on_rtype_and_rid_and_created_at"
   end
 
-  create_table "comfy_cms_sites", force: :cascade do |t|
+  create_table "comfy_cms_sites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "label", null: false
     t.string "identifier", null: false
     t.string "hostname", null: false
@@ -106,7 +106,7 @@ ActiveRecord::Schema.define(version: 2018_11_01_154832) do
     t.index ["hostname"], name: "index_comfy_cms_sites_on_hostname"
   end
 
-  create_table "comfy_cms_snippets", force: :cascade do |t|
+  create_table "comfy_cms_snippets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "site_id", null: false
     t.string "label", null: false
     t.string "identifier", null: false
@@ -118,7 +118,7 @@ ActiveRecord::Schema.define(version: 2018_11_01_154832) do
     t.index ["site_id", "position"], name: "index_comfy_cms_snippets_on_site_id_and_position"
   end
 
-  create_table "comfy_cms_translations", force: :cascade do |t|
+  create_table "comfy_cms_translations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "locale", null: false
     t.integer "page_id", null: false
     t.integer "layout_id"
@@ -132,19 +132,19 @@ ActiveRecord::Schema.define(version: 2018_11_01_154832) do
     t.index ["page_id"], name: "index_comfy_cms_translations_on_page_id"
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
+  create_table "friendly_id_slugs", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "slug", null: false
     t.integer "sluggable_id", null: false
     t.string "sluggable_type", limit: 50
     t.string "scope"
     t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, length: { slug: 70, scope: 70 }
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", length: { slug: 140 }
     t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
     t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
-  create_table "profiles", force: :cascade do |t|
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "job_title"
