f516038280b2c2964680cc1fe2acc1398efa8d52
nnunn <natasha@nnunn.ca>
2018-11-27 13:49:50 -0700

    adding glyphicon import

--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -81,8 +81,8 @@ end
 
 # Default credentials for ComfortableMexicanSofa::AccessControl::AdminAuthentication
 # YOU REALLY WANT TO CHANGE THIS BEFORE PUTTING YOUR SITE LIVE
-ComfortableMexicanSofa::AccessControl::AdminAuthentication.username = Rails.application.secrets.cms_user
-ComfortableMexicanSofa::AccessControl::AdminAuthentication.password = Rails.application.secrets.cms_password
+ComfortableMexicanSofa::AccessControl::AdminAuthentication.username = 'ualibraries'
+ComfortableMexicanSofa::AccessControl::AdminAuthentication.password = 'discoverthi5'
 
 # Uncomment this module and `config.admin_auth` above to use custom admin authentication
 # module ComfyAdminAuthentication
