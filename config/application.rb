1d82082817ecfbf017ef07718fb75cd780d29734
nnunn <natasha@nnunn.ca>
2018-12-04 09:16:40 -0700

    adding custom error pages

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module HomeCms
     config.railties_order = [ActiveStorage::Engine, :main_app, :all]
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.exceptions_app = self.routes
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
