5c3c155a3ac429ae5068c7a33043a81b17f519df
nnunn <natasha@nnunn.ca>
2019-10-25 10:56:19 -0600

    updating to use latest jquery from a CDN

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,10 +2,4 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack');
 
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
-}));
-
 module.exports = environment
