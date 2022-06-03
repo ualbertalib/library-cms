52f13d2da1ca367366e86187eb563571c553f5b7
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2022-06-03 14:51:07 -0600

    The new connection handling does not support `connection_handlers` getter and setter.
    Read more about how to migrate at: https://guides.rubyonrails.org/active_record_multiple_databases.html#migrate-to-the-new-connection-handling

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module HomeCms
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.exceptions_app = self.routes
+    config.active_record.legacy_connection_handling = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
