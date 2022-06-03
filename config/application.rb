ce1f5a708c6f800a45669dca20f92cb608f798a7
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2022-06-03 15:58:18 -0600

    Upgrading from Rails 6.1 to Rails 7.0
    https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#upgrading-from-rails-6-1-to-rails-7-0
    
    2.2 Spring
    
    If your application uses Spring, it needs to be upgraded to at least version 3.0.0. Otherwise you'll get
    
    undefined method `mechanism=' for ActiveSupport::Dependencies:Module
    
    Also, make sure config.cache_classes is set to false in config/environments/test.rb.
    
    2.11 Digest class for ActiveSupport::Digest changing to SHA256
    
    The default digest class for ActiveSupport::Digest is changing from SHA1 to SHA256. This has consequences for things like Etags that will change and cache keys as well. Changing these keys can have impact on cache hit rates, so be careful and watch out for this when upgrading to the new hash.
    
    2.12 New ActiveSupport::Cache serialization format
    A faster and more compact serialization format was introduced.
    
    To enable it you must set config.active_support.cache_format_version = 7.0
    
    However Rails 6.1 applications are not able to read this new serialization format, so to ensure a seamless upgrade you must first deploy your Rails 7.0 upgrade with config.active_support.cache_format_version = 6.1, and then only once all Rails processes have been updated you can set config.active_support.cache_format_version = 7.0.

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,17 @@ module HomeCms
     # route. Without this file serving routes are inaccessible.
     config.railties_order = [ActiveStorage::Engine, :main_app, :all]
     # Initialize configuration defaults for originally generated Rails version.
+    # TODO: needs to be deployed with the following two lines, then can update `config.load_defaults 7.0` and remove the 
+    # `cache_format_version`
     config.load_defaults 6.1
+    config.active_support.cache_format_version = 7.0
+
     config.exceptions_app = self.routes
     config.active_record.legacy_connection_handling = false
+
+    # Active Storage default variant processor changed to :vips 
+    # TODO: evalutate if we're ready for the change to vips
+    Rails.application.config.active_storage.variant_processor = :mini_magick
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
