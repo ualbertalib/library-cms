5725c1e034ed38af0836269c7685952ea2174ecf
Shane Murnaghan <1930474+murny@users.noreply.github.com>
2024-06-10 23:56:24 -0600

    Add tests for staff profiles page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,39 +1,39 @@
 class ProfilesController < ApplicationController
-	$units = {:access => "Access Services", 
-		:archives => "Archives", 
+	$units = {:access => "Access Services",
+		:archives => "Archives",
 		:bib => "Cataloguing Strategies",
 		:collections => "Collection Strategies",
 		:copyright => "Copyright",
-		:digital => "Digital Production & Preservation Services", 
-		:digrepo => "Digital Repository & Data Services", 
+		:digital => "Digital Production & Preservation Services",
+		:digrepo => "Digital Repository & Data Services",
 		:dsc => "Digital Scholarship Centre",
-		:facilities => "Facilities", 
-              	:health => "Faculty Engagement (Health Sciences)", 
+		:facilities => "Facilities",
+              	:health => "Faculty Engagement (Health Sciences)",
 		:science => "Faculty Engagement (Natural + Applied Sciences)",
-		:humanities => "Faculty Engagement (Social Sciences + Humanities)", 
-		:iss => "Information Services & User Engagement", 
-		:admin => "Library Administration", 
+		:humanities => "Faculty Engagement (Social Sciences + Humanities)",
+		:iss => "Information Services & User Engagement",
+		:admin => "Library Administration",
               	:lad => "Library Application Development",
-		:las => "Library Application Support", 
-		:metadata => "Metadata Strategies", 
+		:las => "Library Application Support",
+		:metadata => "Metadata Strategies",
 		:open => "Open Publishing & Digitization Services",
 		:rdm => "Research Data Management",
 		:researchimpact => "Research Impact",
-		:its => "Specialized Technical Support", 
-		:special => "Special Collections", 
+		:its => "Specialized Technical Support",
+		:special => "Special Collections",
 		:stratigic => "Strategic Partnerships",
 		:tl => "Teaching & Learning",
 		:ux => "User Experience"}
-    $buildings = {:augustana => "Augustana Campus Library", 
-	    :bsj => "Bibliothèque Saint-Jean", 
-	    :bpsc=> "Bruce Peel Special Collections", 
-	    :cameron => "Cameron Library", 
-	    :sperber => "Sperber Library", 
-	    :rcrf => "Research & Collections Resource Facility", 
-	    :rutherford => "Rutherford Library", 
+    $buildings = {:augustana => "Augustana Campus Library",
+	    :bsj => "Bibliothèque Saint-Jean",
+	    :bpsc=> "Bruce Peel Special Collections",
+	    :cameron => "Cameron Library",
+	    :sperber => "Sperber Library",
+	    :rcrf => "Research & Collections Resource Facility",
+	    :rutherford => "Rutherford Library",
 	    :stjosephs => "St. Joseph's Library" }
 
-        # You'll have to define "cmsPassword" in secrets.yml, or this will fail. Thanks, ansible. 
+        # You'll have to define "cmsPassword" in secrets.yml, or this will fail. Thanks, ansible.
 	http_basic_authenticate_with name: Rails.application.secrets.cms_user, password: Rails.application.secrets.cms_password, except: [:index, :show]
 
 	def index
@@ -57,8 +57,8 @@ class ProfilesController < ApplicationController
     		format.csv { send_data @profiles.to_csv }
   		end
   	end
-	
-	def show	
+
+	def show
 			@profile = Profile.friendly.find(params[:id])
 	end
 
@@ -83,7 +83,7 @@ class ProfilesController < ApplicationController
 
 	def update
   		@profile = Profile.friendly.find(params[:id])
- 
+
 		if @profile.update(profile_params)
 			redirect_to @profile
 		else
@@ -94,7 +94,7 @@ class ProfilesController < ApplicationController
 	def destroy
   		@profile = Profile.friendly.find(params[:id])
   		@profile.destroy
- 
+
   		redirect_to profiles_path
 	end
 
@@ -106,7 +106,7 @@ class ProfilesController < ApplicationController
 
 
 	private
-	
+
 	def profile_params
 		params.require(:profile).permit(:first_name,:last_name, :job_title, :unit, :email, :phone, :campus_address, :expertise, :introduction, :publications, :staff_since, :liason, :links, :orcid, :committees, :personal_interests, :opt_in)
 	end
