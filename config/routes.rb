eb0cfc285de9146a8fe9eff180cfa4073a1950ec
murny <1930474+murny@users.noreply.github.com>
2024-07-26 09:52:04 -0600

    Add redirect from old staff pages to new staff pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   comfy_route :cms_admin, path: "/admin"
 
+  get "/staff", to: redirect("https://guides.library.ualberta.ca/az/library-staff-directory")
+
   match "/404", to: "errors#file_not_found", via: :all
   match "/422", to: "errors#unprocessable", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
