a7e00a7d788d36a91cd2b3d3f3242d3ce0bf21c9
murny <1930474+murny@users.noreply.github.com>
2024-08-02 16:42:00 -0600

    Update tests after the new CMS data changes

--- test/integration/cms_test.rb
+++ test/integration/cms_test.rb
@@ -9,19 +9,19 @@ class CmsTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "h2", "Search the Library"
   end
-
-  test "hours and locations page" do
-    get comfy_cms_render_page_path(cms_path: "hours-locations")
+  test "ask us page" do
+    get comfy_cms_render_page_path(cms_path: "ask-us")
 
     assert_response :success
-    assert_select "h1", /Hours & Locations/
+    assert_select "h1", /Ask Us/
+    assert_select "h2", "Chat with Us"
   end
 
-  test "about us page" do
-    get comfy_cms_render_page_path(cms_path: "about-us")
+  test "about page" do
+    get comfy_cms_render_page_path(cms_path: "about")
 
     assert_response :success
-    assert_select "h1", /About Us/
+    assert_select "h1", /About the Library/
     assert_select "h2", "A Message from our Chief Librarian"
   end
 end
