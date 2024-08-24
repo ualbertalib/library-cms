6cb4eb6c23571261979346e751b0a6902742b7ba
Shane Murnaghan <1930474+murny@users.noreply.github.com>
2024-08-23 23:30:32 -0600

    Fix admins crud tests

--- test/controllers/admin/admins_controller_test.rb
+++ test/controllers/admin/admins_controller_test.rb
@@ -4,41 +4,24 @@ require_relative "../../test_helper"
 
 class Admin::AdminsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @admin = admins(:default)
-  end
-
-  # Vanilla CMS has BasicAuth, so we need to send that with each request.
-  # Change this to fit your app's authentication strategy.
-  # Move this to test_helper.rb
-  def r(verb, path, options = {})
-    headers = options[:headers] || {}
-    headers["HTTP_AUTHORIZATION"] =
-      ActionController::HttpAuthentication::Basic.encode_credentials(
-        ComfortableMexicanSofa::AccessControl::AdminAuthentication.username,
-        ComfortableMexicanSofa::AccessControl::AdminAuthentication.password
-      )
-    options[:headers] = headers
-    send(verb, path, options)
+    @admin = admins(:admin)
+    sign_in @admin
   end
 
   def test_get_index
-    r :get, admin_admins_path
+    get admin_admins_path
 
     assert_response :success
-    assert assigns(:admins)
-    assert_template :index
   end
 
   def test_get_show
-    r :get, admin_admin_path(@admin)
+    get admin_admin_path(@admin)
 
     assert_response :success
-    assert assigns(:admin)
-    assert_template :show
   end
 
   def test_get_show_failure
-    r :get, admin_admin_path("invalid")
+    get admin_admin_path("invalid")
 
     assert_response :redirect
     assert_redirected_to action: :index
@@ -46,79 +29,74 @@ class Admin::AdminsControllerTest < ActionDispatch::IntegrationTest
   end
 
   def test_get_new
-    r :get, new_admin_admin_path
+    get new_admin_admin_path
 
     assert_response :success
-    assert assigns(:admin)
-    assert_template :new
     assert_select "form[action='/admin/admins']"
   end
 
   def test_get_edit
-    r :get, edit_admin_admin_path(@admin)
+    get edit_admin_admin_path(@admin)
 
     assert_response :success
-    assert assigns(:admin)
-    assert_template :edit
     assert_select "form[action='/admin/admins/#{@admin.id}']"
   end
 
   def test_creation
     assert_difference "Admin.count" do
-      r :post, admin_admins_path, params: {admin: {
-        name: "test name"
+      post admin_admins_path, params: {admin: {
+        email: "new_admin@example.com",
+        password: "password"
       }}
       admin = Admin.last
 
       assert_response :redirect
       assert_redirected_to action: :show, id: admin
-      assert_equal "Admin created", flash[:success]
+      assert_equal "Admin created successfully", flash[:success]
     end
   end
 
   def test_creation_failure
     assert_no_difference "Admin.count" do
-      r :post, admin_admins_path, params: {admin: {}}
+      post admin_admins_path, params: {admin: {}}
 
       assert_response :success
-      assert_template :new
-      assert_equal "Failed to create Admin", flash[:danger]
+      assert_equal "Admin creation failed", flash[:danger]
     end
   end
 
   def test_update
-    r :put, admin_admin_path(@admin), params: {admin: {
-      name: "Updated"
+    put admin_admin_path(@admin), params: {admin: {
+      email: "new_email@example.com"
     }}
 
     assert_response :redirect
     assert_redirected_to action: :show, id: @admin
-    assert_equal "Admin updated", flash[:success]
+    assert_equal "Admin updated successfully", flash[:success]
     @admin.reload
 
-    assert_equal "Updated", @admin.name
+    assert_equal "new_email@example.com", @admin.email
   end
 
   def test_update_failure
-    r :put, admin_admin_path(@admin), params: {admin: {
-      name: ""
+    put admin_admin_path(@admin), params: {admin: {
+      email: ""
     }}
 
     assert_response :success
-    assert_template :edit
-    assert_equal "Failed to update Admin", flash[:danger]
+    assert_equal "Admin update failed", flash[:danger]
     @admin.reload
 
-    assert_not_equal "", @admin.name
+    assert_not_equal "", @admin.email
   end
 
   def test_destroy
     assert_difference "Admin.count", -1 do
-      r :delete, admin_admin_path(@admin)
+      delete admin_admin_path(@admin)
 
       assert_response :redirect
       assert_redirected_to action: :index
-      assert_equal "Admin deleted", flash[:success]
+      assert_equal "Admin deleted successfully", flash[:success]
     end
   end
 end
