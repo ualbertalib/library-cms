f517bef9eac97a1577da370c47a49a9862098db8
Jeffery Antoniuk <jeffery.antoniuk@ualberta.ca>
2026-09-01 11:00:32 -0600

    Update tests and readme after partial  migration; address #1028.

--- test/integration/cms_test.rb
+++ test/integration/cms_test.rb
@@ -9,19 +9,32 @@ class CmsTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "h2", "Search the Library"
   end
-  test "ask us page" do
-    get comfy_cms_render_page_path(cms_path: "ask-us")
+
+  test "archives" do
+    get comfy_cms_render_page_path(cms_path: "archives")
 
     assert_response :success
-    assert_select "h1", /Ask Us/
-    assert_select "h2", "Chat with Us"
+    assert_select "h2", "University of Alberta Archives"
   end
 
-  test "about page" do
-    get comfy_cms_render_page_path(cms_path: "about")
+  test "Peel" do
+    get comfy_cms_render_page_path(cms_path: "peel")
 
     assert_response :success
-    assert_select "h1", /About the Library/
-    assert_select "h2", "A Message from our Chief Librarian"
+    assert_select "h1", "Peel's Prairie Provinces"
+    assert_select "h2", "Peel's Prairie Provinces"
+  end
+
+  test "comfy admin CSS loads" do
+    get "/assets/comfy/admin/cms/application.css"
+
+    assert_response :success
+    assert_includes response.body, "comfy"
+  end
+
+  test "layout CSS english-main loads" do
+    get "/cms-css/1/english-main.css"
+
+    assert_response :success
   end
 end
